52dd4e964cdc5cbe09d8b34202149dd60243f7e6
themis1986 <themis_1986@hotmail.com>
2022-08-17 14:27:22 +0300

    Add mock list of rendered items in the App component and basic styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import "./categories.styles.scss";
+
 const App = () => {
-  return <div>Hello World</div>;
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Women",
+    },
+    {
+      id: 5,
+      title: "Men",
+    },
+  ];
+
+  return (
+    <div className="categories-container">
+      {categories.map(({ id, title }) => {
+        return (
+          <div className="category-container" key={id}>
+            <div className="background-image"></div>
+            <div className="category-body-container">
+              <h2>{title}</h2>
+              <p>Shop now</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default App;
