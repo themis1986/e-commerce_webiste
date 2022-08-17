675c66c93e03a83a254230ba3750851d2a1cdcd9
themis1986 <themis_1986@hotmail.com>
2022-08-17 17:46:54 +0300

    Add firebase functionality for authenticating via Google account and save logged user in the firestore db

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDNS3CgOQOpO9yvqtrPj2ylykeZvc2ftzM",
@@ -25,3 +26,30 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+//   console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+//   console.log(userSnapshot);
+//   console.log(userSnapshot.exists());
+
+  //if user data does not exist
+  //create / set the document with the data from userAuth in my collection
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+  //if data exists
+  //return userDocRef
+  return userDocRef;
+};
